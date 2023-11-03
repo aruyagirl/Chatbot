b376d9c2f6b08e2876ece97bc5d074bb8fff0e90
aruyagirl <baneyzhao@192-168-1-116.tpgi.com.au>
2023-11-03 22:10:30 +1100

    update

--- kittybot-site/components/kittybot.tsx
+++ kittybot-site/components/kittybot.tsx
@@ -4,18 +4,30 @@ import React from "react";
 import Form from "./form";
 import Results from "./results";
 
-export const handler = async (event: any) => {
-    const response = {
-        statusCode: 200,
-        headers: {
-            "Access-Control-Allow-Headers" : "Content-Type",
-            "Access-Control-Allow-Origin": "https://kittybot.vercel.app",
-            "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
-        },
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
-  };
+const allowCors = (fn: { (req: any, res: any): void; (arg0: any, arg1: any): any; }) => async (req: { method: string; }, res: { setHeader: (arg0: string, arg1: string | boolean) => void; status: (arg0: number) => { (): any; new(): any; end: { (): void; new(): any; }; }; }) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (req.method === 'OPTIONS') {
+      res.status(200).end()
+      return
+    }
+    return await fn(req, res)
+  }
+  
+  const handler = (req: any, res: { end: (arg0: string) => void; }) => {
+    const d = new Date()
+    res.end(d.toString())
+  }
+  
+  module.exports = allowCors(handler)
+  
 
 const KittyBot: React.FC = () => {
     const CHARACTER_LIMIT: number = 30;
